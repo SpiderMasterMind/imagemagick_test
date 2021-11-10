8ebb2e5656f11e20b8300f8102c22b48a03680e0
SpiderMasterMind <ben@biggerpockets.com>
2021-11-10 12:00:09 +0000

    fixup naming and implement local storage uploading

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 
-class Users < ApplicationController
-  def new; end
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  
+  end
+
+  def create
+    @user = User.new(name: user_params[:user])
+    @user.avatar = user_params[:avatar]
+
+    if @user.save
+      redirect_to(@user, :notice => 'User was successfully created.')
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :avatar)
+  end
 end
